2f08e8be3752affaa6c472bd65ecb1e319631eb3
Juanmatch <juancruzmatijasevich2016@gmail.com>
2024-02-27 21:08:25 -0300

    nose

--- ProductoWin.cpp
+++ ProductoWin.cpp
@@ -1,11 +1,25 @@
 #include "ProductoWin.h"
+#include "Store.h"
+#include "ProductoWin.h"
+#include "Product.h"
+#include "Utils.h"
 
-ProductoWin::ProductoWin(wxWindow *parent) : WxfbProducto(parent) {
+ProductoWin::ProductoWin(wxWindow *parent, Store *store) :
+	WxfbProducto(parent), m_store(store) {
 	
 }
 
 void ProductoWin::OnClickAgregarProducto( wxCommandEvent& event )  {
-	event.Skip();
+	string nombre = wx_to_std(m_nombre->GetValue());
+	string marca = wx_to_std(m_marca->GetValue());
+	string auxprecio = wx_to_std(m_precio->GetValue());
+	string auxcantidad = wx_to_std(m_cantidad->GetValue());
+	float precio = stof(auxprecio);
+	int cantidad = stoi(auxcantidad);
+	
+	Product temp(nombre,marca,precio,cantidad);
+	m_store->
+	
 }
 
 void ProductoWin::OnClickCancelarProducto( wxCommandEvent& event )  {
