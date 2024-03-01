ab682cc2864c631039c33194434cccb58e4f5534
Franco Carrara <frankko212@gmail.com>
2024-03-01 12:58:41 -0300

    Add validations to seller win

--- SellerWin.cpp
+++ SellerWin.cpp
@@ -3,22 +3,43 @@
 #include "string_conv.h"
 #include "Store.h"
 #include "Seller.h"
+#include <wx/msgdlg.h>
 SellerWin::SellerWin(wxWindow *parent, Store *store) :
 	WxfbVendedor(parent),m_store(store) {
 	
 }
 	
 void SellerWin::OnClickAgregarSeller( wxCommandEvent& event )  {
-	string nombre = wx_to_std(m_nombre->GetValue())+" "+wx_to_std(m_apellido->GetValue());
 	string tel = wx_to_std(m_telefono->GetValue());
 	string email = wx_to_std(m_email->GetValue());
+	
+	if(m_nombre->IsEmpty()){
+		wxMessageBox("Debes ingresar un nombre", "VENDEDOR");
+		return;
+	}
+	if(m_apellido->IsEmpty()){
+		wxMessageBox("Debes ingresar un apellido", "VENDEDOR");
+		return;
+	}
+	string nombre = wx_to_std(m_nombre->GetValue())+" "+wx_to_std(m_apellido->GetValue());
+	
+	if(m_telefono->IsEmpty()){
+		wxMessageBox("Debes ingresar un telefono", "VENDEDOR");
+		return;
+	}
+	if(m_email->IsEmpty()){
+		wxMessageBox("Debes ingresar un correo electronico", "VENDEDOR");
+		return;
+	}
+	
 	Seller temp(nombre,tel,email);
 	m_store->addSeller(std::move(temp));
 	m_store->saveIndividualData(SELLER);
 	EndModal(1);
 }
 
-void SellerWin::OnClickCancelarSeller( wxCommandEvent& event )  {
-	EndModal(0);}
+void SellerWin::OnClickCancelarSeller ( wxCommandEvent& event )  {
+	EndModal(0);
+}
 
 
